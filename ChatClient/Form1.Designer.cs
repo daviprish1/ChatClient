bfb20779f50e43e86d882014fc337c138a6a6752
daviprish1 <daviprishi@gmail.com>
2015-12-21 12:03:40 +0200

    0.1ver

---
+++ ChatClient/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.tbChat = new System.Windows.Forms.TextBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.lbUsers = new System.Windows.Forms.ListBox();
+            this.btnSendMsg = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLogin);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbPass);
+            this.groupBox1.Controls.Add(this.tbUserName);
+            this.groupBox1.Location = new System.Drawing.Point(9, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(452, 54);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(378, 14);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(68, 30);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pass";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User";
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(232, 19);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.PasswordChar = '*';
+            this.tbPass.Size = new System.Drawing.Size(141, 20);
+            this.tbPass.TabIndex = 0;
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(44, 19);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(141, 20);
+            this.tbUserName.TabIndex = 0;
+            // 
+            // tbChat
+            // 
+            this.tbChat.AcceptsReturn = true;
+            this.tbChat.Location = new System.Drawing.Point(9, 66);
+            this.tbChat.Multiline = true;
+            this.tbChat.Name = "tbChat";
+            this.tbChat.ReadOnly = true;
+            this.tbChat.Size = new System.Drawing.Size(305, 208);
+            this.tbChat.TabIndex = 1;
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.AcceptsReturn = true;
+            this.tbMessage.Location = new System.Drawing.Point(9, 280);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(305, 73);
+            this.tbMessage.TabIndex = 1;
+            // 
+            // lbUsers
+            // 
+            this.lbUsers.FormattingEnabled = true;
+            this.lbUsers.Location = new System.Drawing.Point(320, 66);
+            this.lbUsers.Name = "lbUsers";
+            this.lbUsers.Size = new System.Drawing.Size(141, 199);
+            this.lbUsers.TabIndex = 2;
+            // 
+            // btnSendMsg
+            // 
+            this.btnSendMsg.Location = new System.Drawing.Point(320, 310);
+            this.btnSendMsg.Name = "btnSendMsg";
+            this.btnSendMsg.Size = new System.Drawing.Size(140, 42);
+            this.btnSendMsg.TabIndex = 3;
+            this.btnSendMsg.Text = "Send!";
+            this.btnSendMsg.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 365);
+            this.Controls.Add(this.btnSendMsg);
+            this.Controls.Add(this.lbUsers);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.tbChat);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.TextBox tbChat;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.ListBox lbUsers;
+        private System.Windows.Forms.Button btnSendMsg;
+    }
+}
+
---
+++ ChatClient/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UserClassLibrary;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        ManualResetEvent con = new ManualResetEvent(false);
+        ManualResetEvent sen = new ManualResetEvent(false);
+        ManualResetEvent rec = new ManualResetEvent(false);
+
+        string answer { get; set; }
+
+        void StartClient(string command)
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Loopback, 1024);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.BeginConnect(ep, ConnectCallback, socket);
+            con.WaitOne();
+
+            byte[] request = Encoding.ASCII.GetBytes(command + "|");
+            socket.BeginSend(request, 0, request.Length, 0, SendCallback, socket);
+            sen.WaitOne();
+
+
+            StateObject obj = new StateObject();
+            obj.socket = socket;
+
+            socket.BeginReceive(obj.buffer, 0, obj.Size, 0, ReceiveCallback, obj);
+            rec.WaitOne();
+
+            //Console.WriteLine("Data received {0}", answer);
+            string cmd = answer.Substring(0, answer.IndexOf("$") + 1);
+            answer = answer.Substring(answer.IndexOf("$") + 1);
+            switch (cmd)
+            {
+                case "LoginSuccesCmd$":
+                    {
+                        this.UnlockChat();
+                        //this.StartClient("GetUserListCmd$");
+                        request = Encoding.ASCII.GetBytes("GetUserListCmd$" + "|");
+                        socket.BeginSend(request, 0, request.Length, 0, SendCallback, socket);
+                        sen.WaitOne();
+
+                        StateObject objUsrList = new StateObject();
+                        objUsrList.socket = socket;
+
+                        socket.BeginReceive(objUsrList.buffer, 0, objUsrList.Size, 0, ReceiveCallback, objUsrList);
+                        rec.WaitOne();
+                    }
+                    break;
+
+                case "LoginFailCmd$":
+                    MessageBox.Show("Wrong Pass or User!");
+                    this.tbUserName.Text = "";
+                    this.tbPass.Text = "";
+                    break;
+
+                case "RetUserListCmd$":
+                    string[] users = answer.Split(' ');
+                    this.lbUsers.DataSource = users;
+                    break;
+            }
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+        void ConnectCallback(IAsyncResult res)
+        {
+            try
+            {
+                Socket socket = (Socket)res.AsyncState;
+
+                socket.EndConnect(res);
+                con.Set();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        void SendCallback(IAsyncResult res)
+        {
+            Socket socket = (Socket)res.AsyncState;
+
+            int count = socket.EndSend(res);
+            sen.Set();
+        }
+
+        void ReceiveCallback(IAsyncResult res)
+        {
+            StateObject obj = (StateObject)res.AsyncState;
+            Socket clientSocket = obj.socket;
+
+            int count = clientSocket.EndReceive(res);
+            if (count > 0)
+            {
+                obj.result.Append(Encoding.ASCII.GetString(obj.buffer, 0, count));
+                clientSocket.BeginReceive(obj.buffer, 0, obj.Size, 0, ReceiveCallback, obj);
+
+            }
+            else
+            {
+                if (obj.result.Length > 1)
+                {
+                    answer = obj.result.ToString();
+                }
+                rec.Set();
+            }
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UnlockChat()
+        {
+            this.lbUsers.Enabled = true;
+            this.tbChat.Enabled = true;
+            this.tbMessage.Enabled = true;
+            this.btnSendMsg.Enabled = true;
+
+            this.tbUserName.Enabled = false;
+            this.tbPass.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.lbUsers.Enabled = false;
+            this.tbChat.Enabled = false;
+            this.tbMessage.Enabled = false;
+            this.btnSendMsg.Enabled = false;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string command = "LogToChat$" + this.tbUserName.Text + " " + this.tbPass.Text;
+            this.StartClient(command);
+        }
+
+    }
+
+    class StateObject
+    {
+        public Socket socket = null;
+        public const int size = 1024;
+        public int Size
+        {
+            get { return size; }
+        }
+        public byte[] buffer = new byte[size];
+        public StringBuilder result = new StringBuilder();
+    }
+}
